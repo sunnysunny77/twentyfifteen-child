51e19df52ed26e8cf95c2e2155986f43a367f7c3
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-02-18 22:15:28 +0800

    up

--- functions.php
+++ functions.php
@@ -19,8 +19,24 @@ add_action('wp_enqueue_scripts', 'twentyfifteen_child_enqueue_styles');
 //action to add meta data to head
 function twentyfifteen_child_add_meta_data()
 {
+
 ?>
-  <meta name="keywords" content="Tafe, Wordpress, Child Theme">
+	<meta name="keywords" content="Tafe, Wordpress, Child Theme">
 <?php
 }
 add_action('wp_head', 'twentyfifteen_child_add_meta_data');
+
+
+//filter to add author to singular post
+function twentyfifteen_child_singular_author($content)
+{
+
+	if (in_the_loop() && is_main_query() && is_singular()) {
+		$new_content = '<p id="author">Author: Daniel Costello</p>';
+		$content = $content . $new_content;
+		return $content;
+	}
+
+	return $content;
+}
+add_filter('the_content', 'twentyfifteen_child_singular_author', 1);
