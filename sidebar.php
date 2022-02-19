2e423d325a4af2ea65023e3932ab88efc47bc46b
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-02-19 18:03:38 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -13,7 +13,6 @@ if (has_nav_menu('primary') || has_nav_menu('social') || is_active_sidebar('side
 
 		<?php if (has_nav_menu('primary')) : ?>
 			<nav id="site-navigation" class="main-navigation">
-				<h2>Nav</h2>
 				<?php
 				// Primary navigation menu.
 				wp_nav_menu(
